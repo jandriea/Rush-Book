4322705a1c79965d49b6e958ac66e7343ce3bd2a
Jatty Andriean <jattyandriean@outlook.com>
2020-01-28 14:08:27 +0700

    fix gitignore

--- chapter-03/fibonacci_nth/target/debug/deps/fibonacci_nth.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jatty\Rust\projects\Rust-Book\chapter-03\fibonacci_nth\target\debug\deps\fibonacci_nth.exe: src\main.rs
-
-C:\Users\jatty\Rust\projects\Rust-Book\chapter-03\fibonacci_nth\target\debug\deps\fibonacci_nth.d: src\main.rs
-
-src\main.rs:
